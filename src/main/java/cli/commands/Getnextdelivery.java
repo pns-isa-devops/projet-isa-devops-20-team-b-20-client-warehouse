92c5c0daa151545d904ebd720f8968278d65c6e5
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-04-17 03:04:51 +0200

    pns-isa-devops/projet-isa-devops-20-team-b-20#80 pns-isa-devops/projet-isa-devops-20-team-b-20#81 Create command to get parcels (name : getparcels)
    Bug fix : add catch getnextdelivery with new WSDL

--- src/main/java/cli/commands/Getnextdelivery.java
+++ src/main/java/cli/commands/Getnextdelivery.java
@@ -2,6 +2,7 @@ package cli.commands;
 
 import framework.ShellWarehouse;
 import stubs.delivery.Delivery;
+import stubs.delivery.Exception_Exception;
 
 import java.util.List;
 
@@ -9,14 +10,15 @@ public class Getnextdelivery extends Command {
     @Override
     public void execute(List<String> args) {
         try {
-            Delivery delivery = ((ShellWarehouse) this.shell).getDeliveryServiceAPI().getDeliveryService().getNextDelivery();
-            System.out.println("Next delivery to prepare:"
-                    + "\nNumber: " + delivery.getDeliveryId()
-                    + "\nDrone: " + delivery.getDrone().getDroneId()
-                    + "\nParcel number: " + delivery.getParcel().getParcelId()
-            );
+            Delivery delivery = ((ShellWarehouse) this.shell).getDeliveryServiceAPI().getDeliveryService()
+                    .getNextDelivery();
+            System.out.println("Next delivery to prepare:" + "\nNumber: " + delivery.getDeliveryId() + "\nDrone: "
+                    + delivery.getDrone().getDroneId() + "\nParcel number: " + delivery.getParcel().getParcelId());
         } catch (NullPointerException e) {
             System.err.println("Delivery schedule is empty!");
+        } catch (Exception_Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
--- src/main/java/cli/commands/Getparcels.java
+++ src/main/java/cli/commands/Getparcels.java
@@ -2,11 +2,14 @@ package cli.commands;
 
 import java.util.List;
 
+import framework.ShellWarehouse;
+
 public class Getparcels extends Command {
 
     @Override
     public void execute(List<String> args) {
-        
+        ((ShellWarehouse) this.shell).getDeliveryServiceAPI().getDeliveryService().checkForNewParcels();
+        System.out.println("Parcels updated");
     }
 
     @Override
