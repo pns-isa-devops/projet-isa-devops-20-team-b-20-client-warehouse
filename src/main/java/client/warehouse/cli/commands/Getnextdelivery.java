c3c43731a92d3143cbc6739cbf4bb6d91b7996e0
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-05-08 09:31:52 -1000

    pns-isa-devops/projet-isa-devops-20-team-b-20#132 first shot, doesnt work properly

--- src/main/java/client/warehouse/cli/commands/Getnextdelivery.java
+++ src/main/java/client/warehouse/cli/commands/Getnextdelivery.java
@@ -9,9 +9,14 @@ import stubs.delivery.Delivery;
 public class Getnextdelivery extends Command {
     @Override
     public void execute(List<String> args) {
+        if (args.size() < 1) {
+            System.err.println("Not enough arguments");
+            System.err.println("getnextdelivery <HH:mm> ");
+            return;
+        }
         try {
             Delivery delivery = ((ShellWarehouse) this.shell).getDeliveryServiceAPI().getDeliveryService()
-                    .getNextDelivery();
+                    .getNextDelivery(args.get(0));
             System.out.println("Next delivery to prepare:" + "\nNumber: " + delivery.getDeliveryId() + "\nDrone: "
                     + delivery.getDrone().getDroneId() + "\nParcel number: " + delivery.getParcel().getParcelId());
         } catch (Exception e) {
